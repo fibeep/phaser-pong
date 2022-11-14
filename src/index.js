90300f11f99a6ce80104fabc568b5531b5443d2e
Salo <salomonco@gmail.com>
2022-11-14 09:35:47 -0500

    added config

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const config = {
+    type: Phaser.AUTO,
+    parent: 'game',
+    width: 800,
+    height: 640,
+    scale: {
+        mode: Phaser.Scale.RESIZE,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: false
+        }
+    }
+}
